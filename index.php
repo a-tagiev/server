d63b63ea2a203e5e613f47079c8458ef9d5f620e
a-tagiev <66789705+a-tagiev@users.noreply.github.com>
2020-10-25 22:11:40 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// на какие данные рассчитан этот скрипт
+header("Content-Type: application/json");
+// разбираем JSON-строку на составляющие встроенной командой
+$data = json_decode(file_get_contents("php://input")); 
+echo "Сервер получил следующие данные: имя — $data->name, фамилия — $data->lastname";
